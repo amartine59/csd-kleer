b8636b60381b597f0eb38ff72fe54bd01755caf1
amartinez <amartinez@wawand.co>
2021-10-22 08:36:35 -0500

    first sprint

--- src/codebreaker/codebreaker-ui.spec.js
+++ src/codebreaker/codebreaker-ui.spec.js
@@ -1,26 +1,34 @@
 import fs from "fs";
 describe("Codebreaker", () => {
-  it("muestra una bienvenida", () => {
+  beforeEach(() =>{
     document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
+  });
+
+  it("muestra una bienvenida", () => {
     const h1 = document.querySelector("h1").innerHTML;
     expect(h1).toEqual("Codebreaker");
   });
 
-  it("Arriesgo a ganador", () => {
-    document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
-    require("../codebreaker/presenter");
-    arriesgo("5");
-    const resultado = getResultado();
-    expect(resultado).toEqual("Ganaste!");
-  });
+  it("Generar numero aleatorio", () => {
+    let presenter = require("../codebreaker/presenter");
+
+    expect(presenter.randomNumber).toBeGreaterThanOrEqual(0);
+    expect(presenter.randomNumber).toBeLessThanOrEqual(9);
+  })
+
+  it("Coincidencia entre numero ingresado y numero generado", () => {
+      let presenter = require("../codebreaker/presenter");
+      presenter.randomNumber = 5;
+  
+      arriesgo("5");
+      document.querySelector("#arriesgo").click();
+  
+      const resultado = document.querySelector("#resultado").innerHTML;
+      expect(resultado).toEqual("Ganaste!");
+    });
 
   function arriesgo(numero) {
-    const numero_elem = document.querySelector("#numero");
-    const boton_elem = document.querySelector("#arriesgo");
-    numero_elem.value = numero;
-    boton_elem.click();
-  }
-  function getResultado() {
-    return document.querySelector("#resultado").innerHTML;
+    document.querySelector("#numero").value = numero;
+    document.querySelector("#arriesgo").click();
   }
 });
--- src/codebreaker/presenter.js
+++ src/codebreaker/presenter.js
@@ -1,8 +1,12 @@
-const numero_elem = document.querySelector("#numero");
-const boton_elem = document.querySelector("#arriesgo");
-const resultado = document.querySelector("#resultado");
+var randomNumber = Math.floor(Math.random() * 10); // Random number between 0 and 9
 
-boton_elem.addEventListener("click", (event) => {
-  event.preventDefault();
-  resultado.innerHTML = "Ganaste!";
-});
+export function main (){
+  const numero_elem = document.querySelector("#numero");
+  const boton_elem = document.querySelector("#arriesgo");
+  const resultado = document.querySelector("#resultado");
+
+  boton_elem.addEventListener("click", (event) => {
+    event.preventDefault();
+    resultado.innerHTML = "Ganaste!";
+  });
+}
